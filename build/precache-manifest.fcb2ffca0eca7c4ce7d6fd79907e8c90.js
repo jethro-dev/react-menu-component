3e34b83a2512abe1b20e47dbd33b5ae9f81b0f1b
Jethro Au <supergalong@gmail.com>
2021-09-22 17:19:54 +0100

    first commit

---
+++ build/precache-manifest.fcb2ffca0eca7c4ce7d6fd79907e8c90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b19ddc23a8e085a872fbe8e85414345",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5400b7bb5a2a37cc690d",
+    "url": "/static/css/main.26e112f5.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5400b7bb5a2a37cc690d",
+    "url": "/static/js/main.0738e6fb.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
